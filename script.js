27b57cd155605a429203c86bf7ce93fd392f6cd8
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-24 12:43:56 -0400

    Updated forecast to show 5 days

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ $(document).ready(function () {
     var dashboard = $(".dashboard");
     var forecast = $(".forecast");
     var citynames = [];
+    var clear =
+        "https://www.clipartmax.com/png/middle/24-248320_sunny-weather-symbol-transparent.png";
+    var cloudy =
+        "https://c7.uihere.com/files/46/200/53/t-shirt-cloud-weather-clip-art-vector-cloudy-weather-forecast-icon-material.jpg";
+    var rain =
+        "https://www.pinclipart.com/picdir/middle/27-270336_clouds-weather-rain-rain-clip-art-png-transparent.png";
+
+
 
     $("#button").on("click", function (event) {
         event.preventDefault();
@@ -23,6 +31,7 @@ $(document).ready(function () {
             console.log(response);
             mainDash();
             renderButton();
+            getForecast();
 
             // main dashboard weather
             function mainDash() {
@@ -66,10 +75,49 @@ $(document).ready(function () {
                     var button = $("<button>").attr("data-name", citynames[i]);
                     button.addClass("button");
                     button.text(citynames[i]);
-                    buttonArea.append(button);
+                    buttonSpace.append(button);
                 }
             }
+            function getForecast() {
+                var divNames = ["div1", "div2", "div3", "div4", "div5"];
+                forecast.empty();
 
+                for (var i = 0; i < divNames.length; i++) {
+                    var divs = $("<div>");
+                    divs.addClass("forecastDiv");
+                    divs.attr("ID", divNames[i]);
+                    var image = $(
+                        "<img src = '' data-rain = 'https://www.pinclipart.com/picdir/middle/27-270336_clouds-weather-rain-rain-clip-art-png-transparent.png' data-clear = 'https://www.clipartmax.com/png/middle/24-248320_sunny-weather-symbol-transparent.png' data-cloudy = 'https://c7.uihere.com/files/46/200/53/t-shirt-cloud-weather-clip-art-vector-cloudy-weather-forecast-icon-material.jpg' data-status = 'none'>"
+                    );
+                    forecast.append(divs);
+                    divs.append(image);
+                }
+
+            }
+            var div1 = $("#div1");
+            var formattedDate1 = moment(response.list[8].dt_txt).format("MMMM Do");
+            var date1 = $("<p>").text(formattedDate1);
+            div1.append(date1);
+
+            var div2 = $("#div2");
+            var formattedDate2 = moment(response.list[16].dt_txt).format("MMMM Do");
+            var date2 = $("<p>").text(formattedDate2);
+            div2.append(date2);
+
+            var div3 = $("#div3");
+            var formattedDate3 = moment(response.list[24].dt_txt).format("MMMM Do");
+            var date3 = $("<p>").text(formattedDate3);
+            div3.append(date3);
+
+            var div4 = $("#div4");
+            var formattedDate4 = moment(response.list[32].dt_txt).format("MMMM Do");
+            var date4 = $("<p>").text(formattedDate4);
+            div4.append(date4);
+
+            var div5 = $("#div5");
+            var formattedDate5 = moment(response.list[40].dt_txt).format("MMMM Do");
+            var date5 = $("<p>").text(formattedDate5);
+            div5.append(date5);
 
 
         });
