2301e170559887570e1223c17f9adc7ad8054c49
Kendall Masterson <masterson.kendall@gmail.com>
2020-10-24 11:28:24 -0400

    Added api key and button function

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-var notificationA
+$(document).ready(function () {
+    var dashboard = $(".dashboard");
+    var forecast = $(".forecast");
+  
+    $("#button").on("click", function () {
+      var cityName = html($("input"));
+      var key = "d4509dfb81dd238fb34f3ecd8571fb73";
+      var queryURL =
+        "https://api.openweathermap.org/data/2.5/forecast?q=" +
+        cityName +
+        "&appid=" +
+        key;
+  
+      $.ajax({
+        url: queryURL,
+        method: "GET",
+      }).then(function (response) {
+        console.log("hello");
+        console.log(response);
+      });
+    });
+  }); 
